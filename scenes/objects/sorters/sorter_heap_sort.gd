2cacbed02c55c2a4efb3265777b7cb1ef0bd26c3
Gaëtan Staquet <gaetan.staquet@gmail.com>
2023-03-14 12:15:26 +0100

    Heap sort sorts biggest to smallest

--- scenes/objects/sorters/sorter_heap_sort.gd
+++ scenes/objects/sorters/sorter_heap_sort.gd
@@ -20,42 +20,39 @@ var _heap_arr : Array
 func setup(data_size : int, priority_callback : FuncRef):
 	.setup(data_size, priority_callback)
 	
-	_index = 0
+	_index = data_size
 	
 	_heap_arr.clear()
-	_heap_arr.resize(_data_size)
-	for i in _data_size: _heap_arr[i] = i
+	_heap_arr.resize(_data_size + 1)
+	for i in _data_size: _heap_arr[i + 1] = i
 	_heapify(_heap_arr)
 
 # override
 func next_step() -> Dictionary:
-	if _index == _data_size: return {"done":true}
+	if _index == 0: return {"done": true}
+
+	var largest_idx := _h_pop_root(_heap_arr)
 	
-	var smallest_idx : int = _h_pop_root(_heap_arr)
-	
-	# once the smallest index is poped, and the visualizer switches the 2 children
-	# we update the index in _heap_arr to sync with the content of the visualizer
-	var index_pos_in_heap : int = _heap_arr.find(_index)
+	# # once the largest index is poped, and the visualizer switches the 2 children
+	# # we update the index in _heap_arr to sync with the content of the visualizer
+	var index_pos_in_heap : int = _heap_arr.find(_index - 1)
 	if index_pos_in_heap != -1:
-		_heap_arr[index_pos_in_heap] = smallest_idx
+		_heap_arr[index_pos_in_heap] = largest_idx
 	
-	_index += 1
+	_index -= 1
 	
-	if smallest_idx == _index-1:
-		return next_step()
-	else:
-		return {"done":false, "action":SortAction.switch, "indexes":[_index-1, smallest_idx]}
+	return {"done":false, "action":SortAction.switch, "indexes":[_index, largest_idx]}
 
 # override
 func skip_to_last_step() -> Array:
 	var indexes : Array
-	indexes.resize(_data_size)
-	for i in _data_size: indexes[i] = i
+	indexes.resize(_data_size + 1)
+	for i in _data_size: indexes[i + 1] = i
 	_heapify(indexes)
 	
 	var sorted_arr : Array
 	sorted_arr.resize(_data_size)
-	for i in sorted_arr.size():
+	for i in range(sorted_arr.size() - 1, -1, -1):
 		sorted_arr[i] = _h_pop_root(indexes)
 	
 	return sorted_arr
@@ -63,17 +60,17 @@ func skip_to_last_step() -> Array:
 func _heapify(arr : Array):
 	# NOTE: this is a min-heap implementation, meaning the root is the smallest value
 	# start from the middle, because the middle point is guaranteed to be the last non-leaf node
-	for i in range((arr.size()/2)-1, -1, -1):
+	for i in range(arr.size()/2, 0, -1):
 		_h_sift_down(arr, i)
 
 func _h_pop_root(arr : Array) -> int:
-	var root_idx : int = arr[0]
+	var root_idx : int = arr[1]
 	
 	# swap root with last element, then descend new root down the tree untill it's in the right spot
-	Utility.swap_elements(arr, 0, arr.size()-1)
+	Utility.swap_elements(arr, 1, arr.size()-1)
 	# delete previous root
 	arr.remove(arr.size()-1)
-	_h_sift_down(arr, 0)
+	_h_sift_down(arr, 1)
 	
 	return root_idx
 
@@ -86,31 +83,26 @@ func _h_sift_down(arr : Array, index : int): # no I didn't loose my teeth, it's 
 	var left_child : int
 	var right_child : int
 	while true:
-		left_child = index * 2 + 1
-		if left_child >= arr.size(): left_child = -1
+		left_child = index * 2
+		# if left_child >= arr.size(): left_child = -1
 		
-		right_child = index * 2 + 2
-		if right_child >= arr.size(): right_child = -1
-		
-		# find the smallest child if any
-		var smallest_idx : int
-		if left_child == -1 && right_child == -1:
-			break
-		elif left_child == -1:
-			smallest_idx = right_child
-		elif right_child == -1:
-			smallest_idx = left_child
+		right_child = index * 2 + 1
+		# if right_child >= arr.size(): right_child = -1
+
+		var largest_index: int
+		#								arr[left_child] > arr[index]
+		if left_child < arr.size() and _priority_callback.call_func(arr[left_child], arr[index]):
+			largest_index = left_child
 		else:
-			if _priority_callback.call_func(arr[right_child], arr[left_child]):
-				smallest_idx = left_child
-			else:
-				smallest_idx = right_child
+			largest_index = index
 		
-		# swap with the smallest child
-		
-		if (_priority_callback.call_func(arr[index], arr[smallest_idx])):
-			Utility.swap_elements(arr, index, smallest_idx)
-			index = smallest_idx
+		if right_child < arr.size() and _priority_callback.call_func(arr[right_child], arr[largest_index]):
+			largest_index = right_child
+
+		# Swap with the largest child
+		if largest_index != index:
+			Utility.swap_elements(arr, index, largest_index)
+			index = largest_index
 			continue
 		
 		# if we reach this, both children are bigger
